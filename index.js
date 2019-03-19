b53163231f334e8aca1d2a513204bafeefefa11f
Ronak Parekh <rparekh1996@gmail.com>
2019-03-19 19:12:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function dwarfRollCall(dwarves) {
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+ function dwarfRollCall(dwarves) {
+  var array =[]
+   for (var i=0;i<dwarves.length;i++){
+     array.push(`${i+1}. ${dwarves[i]} `)
+   }
+     return array.join("")
 }
-
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  var planeteerCalls2 = [];
+    for (var i = 0; i < planeteerCalls.length; i++ ){
+      planeteerCalls2.push(planeteerCalls[i].toUpperCase() + '!');
+  }
+      return planeteerCalls2
 }
-
-function longPlaneteerCalls(words) {
+var words = ["wind", "fire"]
+ function longPlaneteerCalls(words) {
+    for(var i=0;i<words.length;i++){
+      if (words[i].length>4){
+      return true
+    }
+      return false
+    }
+  
 }
-
-function findTheCheese (foods) {
+function findTheCheese(cheddarCheese){
+  if (cheddarCheese.includes("cheddar"))
+  return "cheddar";
+  else if(cheddarCheese.includes("gouda"))
+  return"gouda";
+  else if(cheddarCheese.includes("camembert"))
+  return "camembert";
+  else 
+  return("no cheese!");
 }
+  
